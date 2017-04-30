fedb01dd010dec0849fe69ff778b26c3a5fbca70
eqbal <info@eqbalq.com>
2017-04-30 11:52:23 +0300

    Implement CalculateRMSE based on USDC quizz

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,23 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+
+  rmse << 0,0,0,0;
+
+  //accumulate squared residuals
+  for(int i=0; i < estimations.size(); ++i){
+    VectorXd residuals = estimations[i] - ground_truth[i];
+    residuals = residuals.array() * residuals.array();
+    rmse += residuals;
+  }
+
+  //calculate the mean
+  rmse = rmse / estimations.size();
+
+  //calculate the squared root
+  rmse = sqrt(rmse.array());
+
+  //return the result
+  return rmse;
 }
